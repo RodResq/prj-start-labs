dbbada9c9516a8e3b9d9f40edd679ab9e2a7a0a4
RodResq <rodrigoresque@yahoo.com.br>
2022-07-23 17:27:48 -0300

    updt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent} from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesDeatilsComponent } from './recipes/recipes-deatils/recipes-deatils.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipesDeatilsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
   ],
   imports: [
     BrowserModule
