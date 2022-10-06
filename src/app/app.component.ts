3c1929996ec5e2cd8f9de6348119a307c5f74fda
RodResq <rodrigoresque@yahoo.com.br>
2022-10-05 22:16:14 -0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prj-start-labs';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
