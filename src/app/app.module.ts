9f5061e1d05902905104601dec909232e18b08af
RodResq <rodrigoresque@yahoo.com.br>
2022-10-13 22:49:34 -0300

    updt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 import { AppComponent } from './app.component';
+import { DiretivasLabsComponent } from './diretivas-labs/diretivas-labs.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -20,6 +22,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DiretivasLabsComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/diretivas-labs/diretivas-labs.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas-labs',
+  templateUrl: './diretivas-labs.component.html',
+  styleUrls: ['./diretivas-labs.component.css']
+})
+export class DiretivasLabsComponent implements OnInit {
+  // numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  oddNumbers = [1, 2, 3, 4, 5];
+  evenNumbers = [6, 7, 8, 9];
+  onlyOdd = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
