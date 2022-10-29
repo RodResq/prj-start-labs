fa3a5b6a785334bccd399aff2de819ee5eead49b
RodResq <rodrigoresque@yahoo.com.br>
2022-10-29 11:31:11 -0300

    @Diretivas-labs

--- src/app/card-hover/cardhover.directive.ts
+++ src/app/card-hover/cardhover.directive.ts
@@ -1,13 +1,23 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[ccCardhover]'
 })
 export class CardhoverDirective implements OnInit {
-
+  @Input() ccCardhover: string = 'green';
+  @Input() corRetirarMouse: string = this.ccCardhover;
+  
   constructor(private el: ElementRef, private render: Renderer2) { }
   
   ngOnInit(): void {
-    this.render.setStyle(this.el.nativeElement, 'backgroundColor', 'gray');
+    this.render.setStyle(this.el.nativeElement, 'backgroundColor', this.ccCardhover);
+  }
+
+  @HostListener('mouseover') onHover() {
+    this.render.setStyle(this.el.nativeElement, 'backgroundColor', this.ccCardhover)
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.render.setStyle(this.el.nativeElement, 'backgroundColor', this.corRetirarMouse)
   }
 }
