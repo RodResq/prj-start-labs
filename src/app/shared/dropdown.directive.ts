7380ef939aec3bd0dd4717d203e949fcecc8f541
RodResq <rodrigoresque@yahoo.com.br>
2022-11-08 11:30:04 -0300

    updt

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,14 +6,14 @@ import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core"
 export class DropDownDirective {
     @HostBinding('class.open') isOpen = false;
 
-    // @HostListener('click') toggleOpen() {
-    //     this.isOpen = !this.isOpen;
+    @HostListener('click') toggleOpen() {
+        this.isOpen = !this.isOpen;
 
-    // }    
+    }    
 
-    @HostListener('document.click', ['$event']) tootleOpen(event: Event) {
-        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
-    }
+    // @HostListener('document.click', ['$event']) tootleOpen(event: Event) {
+    //     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    // }
 
     constructor(private elRef: ElementRef) {
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,5 +1,7 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
 export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomates', 5),
@@ -11,5 +13,14 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        // for (const ingredient of ingredients) {
+        //     this.addIngredient(ingredient);
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
