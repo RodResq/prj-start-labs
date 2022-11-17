bbc0118b1661f5bbde726f38e8721ce711ff2dcf
RodResq <rodrigoresque@yahoo.com.br>
2022-11-17 16:02:53 -0300

    updt

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
-import { RecipeService } from './recipe.service';
-import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -10,16 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
-  selectedRecipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        } 
-      );
   }
 
 }
