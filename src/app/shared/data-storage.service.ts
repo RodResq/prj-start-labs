e57520d8b08c16dc24631a02b7f695998a63e3cc
RodResq <rodrigoresque@yahoo.com.br>
2023-01-27 11:38:56 -0300

    updt

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,3 +1,5 @@
+import { RecipeService } from './../recipes/recipe.service';
+import { Recipe } from './../recipes/recipe.model';
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -6,5 +8,22 @@ import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'any' })
 export class DataStorageService {
 
-    constructor(private http: HttpClient) {}
+    constructor(
+        private http: HttpClient,
+        private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        return this.http.put('https://ng-course-recipe-book-680f0-default-rtdb.firebaseio.com/rescipes.json', recipes).subscribe(response => {
+            console.log(response);
+            
+        })
+    }
+
+    fetchRecipes() {
+        return this.http.get<Recipe[]>('https://ng-course-recipe-book-680f0-default-rtdb.firebaseio.com/rescipes.json')
+            .subscribe(recipes => {
+                this.recipeService.setRecipes(recipes);
+            });
+    }
 }
