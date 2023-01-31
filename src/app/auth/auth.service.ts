4b0d2834a1d5048a5f3eb9ad2514c1138e8e01c3
RodResq <rodrigoresque@yahoo.com.br>
2023-01-31 11:19:24 -0300

    updt

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+interface AuthResponseDate {
+    idToken: string,
+    email: string,
+    refreshToken: string,
+    expiresIn: string,
+    localId: string,
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    
+    constructor(private http: HttpClient) {}
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseDate>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDRazo2GdMTUYE6ze2QqhzPvvIdDptC9tc',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            } 
+        )
+    }
+
+}
